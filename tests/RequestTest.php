a3e359855036ee9cdbf82b0aa715acd54ea6d24b
stas <stas.dovgodko@corp.olanola.com>
2015-03-02 13:40:19 +0200

    Add tests stubs

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -164,4 +164,556 @@ class RequestTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf(Url::CLASS, $request->getUri());
         $this->assertEquals('http://example2.com/path/path12/path13.ext?x1=y1&x2=y2', $request->getUri());
     }
+
+    /**
+     * @covers wmlib\controller\Request::getSession
+     * @todo   Implement testGetSession().
+     */
+    public function testGetSession()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::hasSession
+     * @todo   Implement testHasSession().
+     */
+    public function testHasSession()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withSession
+     * @todo   Implement testWithSession().
+     */
+    public function testWithSession()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getUserAgent
+     * @todo   Implement testGetUserAgent().
+     */
+    public function testGetUserAgent()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::setInput
+     * @todo   Implement testSetInput().
+     */
+    public function testSetInput()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getInput
+     * @todo   Implement testGetInput().
+     */
+    public function testGetInput()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getCookie
+     * @todo   Implement testGetCookie().
+     */
+    public function testGetCookie()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::isXHR
+     * @todo   Implement testIsXHR().
+     */
+    public function testIsXHR()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getCookies
+     * @todo   Implement testGetCookies().
+     */
+    public function testGetCookies()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getBaseUrl
+     * @todo   Implement testGetBaseUrl().
+     */
+    public function testGetBaseUrl()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getHeader
+     * @todo   Implement testGetHeader().
+     */
+    public function testGetHeader()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getContentType
+     * @todo   Implement testGetContentType().
+     */
+    public function testGetContentType()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getMethod
+     * @todo   Implement testGetMethod().
+     */
+    public function testGetMethod()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getUrl
+     * @todo   Implement testGetUrl().
+     */
+    public function testGetUrl()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getUrlPath
+     * @todo   Implement testGetUrlPath().
+     */
+    public function testGetUrlPath()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getFullUri
+     * @todo   Implement testGetFullUri().
+     */
+    public function testGetFullUri()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::isPost
+     * @todo   Implement testIsPost().
+     */
+    public function testIsPost()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getParam
+     * @todo   Implement testGetParam().
+     */
+    public function testGetParam()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::hasParam
+     * @todo   Implement testHasParam().
+     */
+    public function testHasParam()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::isCheckModifiedSince
+     * @todo   Implement testIsCheckModifiedSince().
+     */
+    public function testIsCheckModifiedSince()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::isCheckNotContain
+     * @todo   Implement testIsCheckNotContain().
+     */
+    public function testIsCheckNotContain()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::__toString
+     * @todo   Implement test__toString().
+     */
+    public function test__toString()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getServerParams
+     * @todo   Implement testGetServerParams().
+     */
+    public function testGetServerParams()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getCookieParams
+     * @todo   Implement testGetCookieParams().
+     */
+    public function testGetCookieParams()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getFileParams
+     * @todo   Implement testGetFileParams().
+     */
+    public function testGetFileParams()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getBodyParams
+     * @todo   Implement testGetBodyParams().
+     */
+    public function testGetBodyParams()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getAttributes
+     * @todo   Implement testGetAttributes().
+     */
+    public function testGetAttributes()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getAttribute
+     * @todo   Implement testGetAttribute().
+     */
+    public function testGetAttribute()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::hasAttribute
+     * @todo   Implement testHasAttribute().
+     */
+    public function testHasAttribute()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getBody
+     * @todo   Implement testGetBody().
+     */
+    public function testGetBody()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::persistParam
+     * @todo   Implement testPersistParam().
+     */
+    public function testPersistParam()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withMethod
+     * @todo   Implement testWithMethod().
+     */
+    public function testWithMethod()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withProtocolVersion
+     * @todo   Implement testWithProtocolVersion().
+     */
+    public function testWithProtocolVersion()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getProtocolVersion
+     * @todo   Implement testGetProtocolVersion().
+     */
+    public function testGetProtocolVersion()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withHeader
+     * @todo   Implement testWithHeader().
+     */
+    public function testWithHeader()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withAddedHeader
+     * @todo   Implement testWithAddedHeader().
+     */
+    public function testWithAddedHeader()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withoutHeader
+     * @todo   Implement testWithoutHeader().
+     */
+    public function testWithoutHeader()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withBody
+     * @todo   Implement testWithBody().
+     */
+    public function testWithBody()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getRequestTarget
+     * @todo   Implement testGetRequestTarget().
+     */
+    public function testGetRequestTarget()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withRequestTarget
+     * @todo   Implement testWithRequestTarget().
+     */
+    public function testWithRequestTarget()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withCookieParams
+     * @todo   Implement testWithCookieParams().
+     */
+    public function testWithCookieParams()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withQueryParams
+     * @todo   Implement testWithQueryParams().
+     */
+    public function testWithQueryParams()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::getParsedBody
+     * @todo   Implement testGetParsedBody().
+     */
+    public function testGetParsedBody()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withParsedBody
+     * @todo   Implement testWithParsedBody().
+     */
+    public function testWithParsedBody()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withAttribute
+     * @todo   Implement testWithAttribute().
+     */
+    public function testWithAttribute()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers wmlib\controller\Request::withoutAttribute
+     * @todo   Implement testWithoutAttribute().
+     */
+    public function testWithoutAttribute()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
 }
