0536673c7712c623e6ac3d273cbdd74e0f59213d
stas.dovgodko <stas.dovgodko@webmaniacs.net>
2015-03-22 20:28:59 +0200

    Draft

--- lib/Route/Controller.php
+++ lib/Route/Controller.php
@@ -207,8 +207,6 @@ class Controller extends Route
         } elseif ($request->hasAttribute($param_name)) {
             return $request->getAttribute($param_name);
         } else {
-            print_r($arguments);
-            die();
             throw new PropertyNotFoundException("Controller action argument $param_name not found");
         }
     }
